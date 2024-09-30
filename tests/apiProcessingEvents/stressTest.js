0d1b226159f8cc3ae6e391b38009494844ddb358
Jorge Eider Vásquez Urrutia <jevasquez681@gmail.com>
2024-09-30 02:52:52 -0500

    Eliminación del test stressTest.js la importación import { SharedArray } from 'k6/data'; y const testData = new SharedArray('testData', function () {                                                                                     return JSON.parse(open('../data/testData.json'));                                                                                 }); de cada uno de los test

--- tests/apiProcessingEvents/stressTest.js
+++ tests/apiProcessingEvents/stressTest.js
@@ -1,12 +1,7 @@
-import { SharedArray } from 'k6/data';
 import { BASE_URL, API_PROCESSING_EVENTS } from '../../config/config.js';
 import { getRequest } from '../../utils/httpClient.js';
 import { stressTestOptions } from '../../config/options/optionsProcessingEvents/stressTestOptions.js';
 import {executeTest} from "../../utils/UtilsTestProcessingEvents";
-
-const testData = new SharedArray('testData', function () {
-    return JSON.parse(open('../data/testData.json'));
-});
 
 export let options = stressTestOptions;
 
